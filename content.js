e7f4440eb7f590141291e3ac8e28af96e6ef8c83
joeelucidat.com <joe@elucidat.com>
2021-05-15 19:43:08 +0100

    Added debounce

--- content.js
+++ content.js
@@ -23,44 +23,6 @@ function getPathTo(element) {
     }
 }
 
-document.addEventListener("mousedown", function (event) {
-    chrome.storage.sync.get(["recording"], function (result) {
-        const isRecording = result.recording;
-        const tagName = event.path[0].tagName.toLowerCase();
-        if (isRecording) {
-            recordedPaths.push({
-                eventType: "clicked",
-                target: event.target,
-                xPath: getPathTo(event.target),
-                value: "",
-                tagName: tagName,
-            });
-        }
-        console.log("recordedPaths", recordedPaths);
-        if (tagName === "input") {
-            var searchTimeout;
-            event.target.addEventListener("keypress", (event) => {
-                if (isRecording) {
-                    if (searchTimeout != undefined) {
-                        clearTimeout(searchTimeout);
-                    }
-                    searchTimeout = setTimeout(callServerScript, 4000);
-                    function callServerScript() {
-                        recordedPaths.push({
-                            eventType: "entered_text",
-                            target: event.target,
-                            xPath: getPathTo(event.target),
-                            value: event.target.value,
-                            tagName: tagName,
-                        });
-                        console.log("recordedPaths", recordedPaths);
-                    }
-                }
-            });
-        }
-    });
-});
-
 const isAllowedTag = (tag) => {
     const allowedTags = [
         "div",
@@ -78,8 +40,77 @@ const isAllowedTag = (tag) => {
     return allowedTags.indexOf(tag) !== -1;
 };
 
+function Path(eventType, target, xPath, value, tagName) {
+    this.eventType = eventType;
+    this.target = target;
+    this.xPath = xPath;
+    this.value = value;
+    this.tagName = tagName;
+}
+
+const getTagName = (event) => {
+    return event.path[0].tagName.toLowerCase();
+};
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+document.addEventListener("mousedown", function (event) {
+    const tagName = getTagName(event);
+    if (isAllowedTag(tagName)) {
+        chrome.storage.sync.get(["recording"], function (result) {
+            const isRecording = result.recording;
+            const tagName = event.path[0].tagName.toLowerCase();
+            if (isRecording) {
+                recordedPaths.push(
+                    new Path(
+                        "clicked",
+                        event.target,
+                        getPathTo(event.target),
+                        "",
+                        tagName
+                    )
+                );
+            }
+            console.log("recordedPaths", recordedPaths);
+        });
+    }
+});
+
+document.addEventListener(
+    "keypress",
+    debounce((event) => {
+        const tagName = getTagName(event);
+        if (tagName === "input") {
+            recordedPaths.push(
+                new Path(
+                    "entered_text",
+                    event.target,
+                    getPathTo(event.target),
+                    event.target.value,
+                    tagName
+                )
+            );
+            console.log("recordedPaths", recordedPaths);
+        }
+    }, 3000)
+);
+
 document.addEventListener("mouseover", function (event) {
-    const tagName = event.path[0].tagName.toLowerCase();
+    const tagName = getTagName(event);
     const tagString = `cucumber_selected_${tagName}`;
     if (isAllowedTag(tagName)) {
         previousSelectedItem &&
